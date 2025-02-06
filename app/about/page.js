1df8003fdb4c10f059c28dff3a5cf6fdb10f7e04
franzieYoogan <franzieyoogan@gmail.com>
2025-02-06 00:13:19 -0300

    first

---
+++ app/about/page.js
@@ -0,0 +1,16 @@
+import Header from "../header/page"
+
+export default function About() {
+
+    return(
+
+        <main>
+
+            <Header></Header>
+        <h1>About</h1>
+
+        </main>
+
+    )
+
+}
